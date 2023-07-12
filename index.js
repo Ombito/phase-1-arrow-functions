3bede3b1b075348df119c5f88cbb4211d5ee2aaf
ombito <alvinombito09@gmail.com>
2023-07-12 15:32:54 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+const divide = function(){
+    return 2000/100;
+}
+divide(2000/100);
+console.log(divide(2000/100))
+
+const square = (x) => x*x 
+    let x = 2;
+    let answer = (square(x))
+    console.log(answer)
+
+const add = (a, b) => {
+    return a + b;
+}
+let a = 3
+let b = 4
+let result = sum(a, b);
+console.log(result)
+
+// function divide(num1, num2){
+//     return num1 / num2
+// }
+// let num1 = 2000;
+//     let num2 = 100;
+//     let results = divide (2000, 100)
+//     console.log(results)
